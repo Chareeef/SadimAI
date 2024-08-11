4cbb8a269cb84cc628071e881ed763503f80c85c
Chareeef <youssef.charif.h@gmail.com>
2024-08-11 01:26:05 +0000

    Set icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Providers from "./components/Providers";
 import "./globals.css";
 import "highlight.js/styles/tomorrow-night-blue.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+
+        <link
+          rel="icon"
+          href="/android-chrome?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </Head>
+
       <body className={`${inter.className}`}>
         <Providers>{children}</Providers>
       </body>
