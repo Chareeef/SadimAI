0051dd3fa97a8bcff167caf4b5a8e32eb32d7afa
Chareeef <youssef.charif.h@gmail.com>
2024-08-11 03:26:57 +0000

    Make the landing page responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,23 +7,23 @@ export default function Header() {
   const { data: session } = useSession();
 
   return (
-    <header className="flex items-center justify-between bg-teal-700 p-2 text-white">
-      <div className="flex items-center font-bold">
+    <header className="flex flex-col md:flex-row items-center justify-between gap-4 bg-teal-700 p-2 text-white text-lg">
+      <div className="flex flex-col md:flex-row items-center font-bold">
         <Image src="/Sadim_Logo.png" alt="Sadim Logo" width={64} height={64} />
-        <h1 className="text-3xl ml-2">Sadim</h1>
+        <h1 className="text-xl md:text-3xl m-2">Sadim</h1>
       </div>
       <nav>
         {session && session.user ? (
           <div className="flex items-center space-x-4">
             <Link
               href="/chat"
-              className="text-2xl p-2 bg-teal-500 hover:bg-teal-600 border-2 border-white rounded-lg"
+              className="md:text-2xl p-2 bg-teal-500 hover:bg-teal-600 border-2 border-white rounded-lg"
             >
               Chat
             </Link>
             <button
               onClick={() => signOut()}
-              className="text-2xl p-2 bg-emerald-500 hover:bg-emerald-600 border-2 border-white rounded-lg"
+              className="md:text-2xl p-2 bg-emerald-500 hover:bg-emerald-600 border-2 border-white rounded-lg"
             >
               Sign Out
             </button>
@@ -31,7 +31,7 @@ export default function Header() {
         ) : (
           <button
             onClick={() => signIn()}
-            className="text-2xl p-2 bg-emerald-500 hover:bg-emerald-600 border-2 border-white rounded-lg"
+            className="md:text-2xl p-2 bg-emerald-500 hover:bg-emerald-600 border-2 border-white rounded-lg"
           >
             Sign In
           </button>
