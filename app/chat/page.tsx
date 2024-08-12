4da529034a04880cce39a9d6fc2cf5a662c83df4
Chareeef <youssef.charif.h@gmail.com>
2024-08-12 04:09:11 +0000

    Make the chat page responsive

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -18,15 +18,20 @@ interface InputAreaProps {
   sendMessage: MouseEventHandler<HTMLButtonElement>;
 }
 
+interface OpenAsideProps {
+  openAside: boolean;
+  setOpenAside: Dispatch<SetStateAction<boolean>>;
+}
+
 function InputArea({
   userMessage,
   setUserMessage,
   sendMessage,
 }: InputAreaProps) {
   return (
-    <div className="h-14 flex border-y-2 border-r-2 border-teal-500 m-0">
+    <div className="h-[3rem] flex border-y-2 border-r-2 border-teal-500 m-0">
       <textarea
-        className="grow h-full pt-3 px-2 bg-teal-600 text-white border-r-2 border-teal-500 focus:outline-none focus:bg-teal-700"
+        className="grow h-full pt-2 px-2 bg-teal-600 text-white border-r-2 border-teal-500 outline-none focus:bg-teal-700"
         value={userMessage}
         onChange={(e) => setUserMessage(e.target.value)}
         placeholder="Type your message here..."
@@ -41,7 +46,7 @@ function InputArea({
   );
 }
 
-function ChatWindow() {
+function ChatWindow({ openAside, setOpenAside }: OpenAsideProps) {
   const initialConversation: Message[] = [];
   const [conversation, setConversation] =
     useState<Message[]>(initialConversation);
@@ -96,11 +101,15 @@ function ChatWindow() {
   }
 
   return (
-    <main className="col-span-3 flex flex-col text-lg bg-white max-h-dvh">
+    <main className="md:col-span-3 flex flex-col text-lg bg-white h-dvh">
+      <button
+        onClick={() => setOpenAside(true)}
+        className={`${openAside && "hidden"} md:hidden absolute  top-[40dvh] left-2 h-0 w-0 border-y-[3dvh] border-y-transparent border-l-[6dvh] border-l-emerald-700/30 hover:border-l-emerald-900`}
+      ></button>
       <div className="grow overflow-y-auto">
-        <div className="flex flex-col justify-end p-2 min-h-full space-y-2">
+        <div className="flex flex-col justify-end p-2 min-h-full space-y-2 text-base">
           {conversation.length === 0 && (
-            <p className="text-base text-gray-700 self-center">
+            <p className="text-base text-gray-700 self-center text-center">
               I am Sadim! Ready to help you with anything you need!
             </p>
           )}
@@ -108,7 +117,7 @@ function ChatWindow() {
           {conversation.map((message, index) => (
             <div
               key={index}
-              className={`message p-2 ${
+              className={`message z-10 p-2 ${
                 message.role === "user"
                   ? "bg-teal-400 text-white self-end rounded-bl-lg"
                   : "bg-teal-500 text-white self-start rounded-br-lg"
@@ -133,30 +142,36 @@ function ChatWindow() {
   );
 }
 
-function Aside() {
+function Aside({ openAside, setOpenAside }: OpenAsideProps) {
   const { data: session } = useSession();
 
   const user = session?.user;
   return (
-    <aside className="flex flex-col bg-teal-800 border-r-2 border-teal-500">
+    <aside
+      className={`absolute md:relative h-dvh w-full z-20 flex flex-col bg-teal-800 border-r-2 border-teal-500 transform md:translate-x-0 ${!openAside && "-translate-x-full"} transiton-transform ease-in-out duration-700`}
+    >
+      <button
+        onClick={() => setOpenAside(false)}
+        className="md:hidden absolute top-[40dvh] right-2 h-0 w-0 border-y-[3dvh] border-y-transparent border-r-[6dvh] border-r-white-500/30 hover:border-r-white-500"
+      ></button>
       {/* Profile */}
-      <div className="flex flex-col items-center text-center justify-around py-2 shadow-lg min-h-[25%]">
+      <div className="flex md:flex-col items-center justify-around text-center py-4 shadow-lg min-h-[20%]">
         <Image
           src={user?.image as string}
           alt="Profile picture"
-          width={64}
-          height={64}
+          width={50}
+          height={50}
           className="rounded-full border-2 border-emerald-400"
         />
         {user?.name !== user?.email ? (
-          <>
-            <p className="text-xl text-white font-mono font-bold italic">
+          <div>
+            <p className="text-[1.25em] text-white font-mono font-bold italic mb-1">
               {user?.name}
             </p>
-            <p className="text-base text-white font-mono font-bold break-all">
+            <p className="text-[1em] text-white font-mono font-bold break-all">
               {user?.email}
             </p>
-          </>
+          </div>
         ) : (
           <p className="text-xl text-white font-mono font-bold italic">
             {user?.email}
@@ -170,10 +185,10 @@ function Aside() {
       </div>
 
       {/* Logout */}
-      <div className="h-14 p-4 flex items-center justify-center">
+      <div className="h-[3rem] p-2 flex items-center justify-center">
         <button
           onClick={() => signOut({ callbackUrl: "/" })}
-          className="rounded bg-rose-500 hover:bg-rose-600 p-2"
+          className="rounded bg-rose-500 hover:bg-rose-600 px-2 py-1 my-2"
         >
           Sign Out
         </button>
@@ -183,10 +198,12 @@ function Aside() {
 }
 
 export default function Chat() {
+  const [openAside, setOpenAside] = useState<boolean>(false);
+
   return (
-    <div className="grid grid-cols-4 gap-0 h-dvh">
-      <Aside />
-      <ChatWindow />
+    <div className="md:grid md:grid-cols-4 md:gap-0 h-dvh">
+      <Aside openAside={openAside} setOpenAside={setOpenAside} />
+      <ChatWindow openAside={openAside} setOpenAside={setOpenAside} />
     </div>
   );
 }
